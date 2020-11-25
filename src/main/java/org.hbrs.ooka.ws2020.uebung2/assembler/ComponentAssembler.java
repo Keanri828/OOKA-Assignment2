cb61c1aea9791fab4cc472fc4c7ea36674e5b720
Robin0815 <robin_frank_ccaa@yahoo.de>
2020-11-25 12:40:55 +0100

    changes

--- src/main/java/org.hbrs.ooka.ws2020.uebung2/assembler/ComponentAssembler.java
+++ src/main/java/org.hbrs.ooka.ws2020.uebung2/assembler/ComponentAssembler.java
@@ -1,4 +1,5 @@
 package org.hbrs.ooka.ws2020.uebung2.assembler;
 
 public class ComponentAssembler {
+
 }
---
+++ src/main/java/org.hbrs.ooka.ws2020.uebung2/component/Component.java
@@ -0,0 +1,4 @@
+package org.hbrs.ooka.ws2020.uebung2.component;
+
+public class Component {
+}
