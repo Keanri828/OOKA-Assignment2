55145a3f8872801c64381690aabb9f107dd43c58
Fiona Brücher <fyobru@gmail.com>
2020-12-02 19:15:24 +0100

    Thread refactoring

--- src/main/java/org.hbrs.ooka.ws2020.uebung2/assembler/ComponentThread.java
+++ src/main/java/org.hbrs.ooka.ws2020.uebung2/assembler/ComponentThread.java
@@ -2,6 +2,7 @@ package org.hbrs.ooka.ws2020.uebung2.assembler;
 
 import org.hbrs.ooka.ws2020.uebung2.component.Component;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class ComponentThread extends Thread{
@@ -29,23 +30,41 @@ public class ComponentThread extends Thread{
     public void run() {
         // Start Objekt und Methode
         Method startMethod = comp.getStart();
-        Class<?> startClass = startMethod.getDeclaringClass();
-        Object startObject = startClass.getDeclaredConstructor().newInstance();
-        Class<?> startReturnType = startMethod.getReturnType(); // if needed
+        Object startObject = instantiateObjectForMethod(startMethod);
+        //Class<?> startReturnType = startMethod.getReturnType(); // if needed
 
         // End Objekt und Methode
         Method endMethod = comp.getEnd();
-        Class<?> endClass = endMethod.getDeclaringClass();
-        Object endObject = endClass.getDeclaredConstructor().newInstance();
-        Class<?> endReturnType = endMethod.getReturnType(); // if needed
+        Object endObject = instantiateObjectForMethod(endMethod);
+        //Class<?> endReturnType = endMethod.getReturnType(); // if needed
 
         try {
             // hier: Generierung eines Objektes der Klasse mit Start-Methode
             startMethod.invoke(startObject);
-        } catch (InterruptedException e) {
+        } catch (InterruptedException | IllegalAccessException | InvocationTargetException e) {
             // hier: ... der Klasse mit End-Methode
-            endMethod.invoke(endObject);
+            try {
+                endMethod.invoke(endObject);
+            } catch (IllegalAccessException | InvocationTargetException illegalAccessException) {
+                illegalAccessException.printStackTrace();
+            }
         }
+    }
+
+    /**
+     * Generate instantiation of an object for given
+     * @param m Method
+     * @return mObject
+     */
+    private Object instantiateObjectForMethod(Method m) {
+        String mClassName = m.getDeclaringClass().getName(); // get Class-name of declaring class
+        Object mObject = null;
+        try {
+            mObject = this.getContextClassLoader().loadClass(mClassName).getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return mObject;
     }
 
     /**
