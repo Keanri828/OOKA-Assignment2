9188414b2486bcdabf0888f688b8f35ab256d3f2
Fiona Brücher <fyobru@gmail.com>
2020-12-02 17:36:06 +0100

    Start/End Objekte im Thread

--- src/main/java/org.hbrs.ooka.ws2020.uebung2/assembler/ComponentThread.java
+++ src/main/java/org.hbrs.ooka.ws2020.uebung2/assembler/ComponentThread.java
@@ -9,9 +9,10 @@ public class ComponentThread extends Thread{
     private volatile boolean stop = false;
     private Component comp;
 
-    public ComponentThread(String name, Component c) {
+    public ComponentThread(String name, Component c, ClassLoader cl) {
         this.setName(name);
         this.comp = c;
+        this.setContextClassLoader(cl);
     }
 
     /**
@@ -27,21 +28,24 @@ public class ComponentThread extends Thread{
      * unterbrochen werden
      */
     public void run() {
+        // Start Objekt und Methode
         Method startMethod = comp.getStart();
         Class<?> startClass = startMethod.getDeclaringClass();
-        // TODO: Objekt generieren via ClassLoader
+        Object startObject = startClass.getDeclaredConstructor().newInstance();
         Class<?> startReturnType = startMethod.getReturnType(); // if needed
-        
+
+        // End Objekt und Methode
         Method endMethod = comp.getEnd();
         Class<?> endClass = endMethod.getDeclaringClass();
-        // TODO: Objekt generieren via ClassLoader
+        Object endObject = endClass.getDeclaredConstructor().newInstance();
         Class<?> endReturnType = endMethod.getReturnType(); // if needed
+
         try {
             // hier: Generierung eines Objektes der Klasse mit Start-Methode
-            startMethod.invoke(); //TODO: Invoke Object?!
+            startMethod.invoke(startObject);
         } catch (InterruptedException e) {
             // hier: ... der Klasse mit End-Methode
-            endMethod.invoke(); //TODO: Invoke Object
+            endMethod.invoke(endObject);
         }
     }
 
