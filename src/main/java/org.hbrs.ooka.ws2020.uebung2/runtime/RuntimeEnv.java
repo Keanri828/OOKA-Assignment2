3d21d0d2e6308f087b5755f97ac8077de086984b
Robin0815 <robin_frank_ccaa@yahoo.de>
2020-12-02 22:04:54 +0100

    alot

--- src/main/java/org.hbrs.ooka.ws2020.uebung2/runtime/RuntimeEnv.java
+++ src/main/java/org.hbrs.ooka.ws2020.uebung2/runtime/RuntimeEnv.java
@@ -1,6 +1,7 @@
 package org.hbrs.ooka.ws2020.uebung2.runtime;
 
 import org.hbrs.ooka.ws2020.uebung2.assembler.ComponentAssembler;
+import org.hbrs.ooka.ws2020.uebung2.assembler.ComponentThread;
 import org.hbrs.ooka.ws2020.uebung2.component.Component;
 
 import java.util.ArrayList;
@@ -13,6 +14,8 @@ public class RuntimeEnv {
     public void initComponent(String name) throws Exception{
         ComponentAssembler comAss = new ComponentAssembler();
         Component com = comAss.loadClasses("Counter");
+        ComponentThread thread = new ComponentThread(name, com);
+
         String[] param = new String[0];
         System.out.println(com.getStart().getName());
         com.getStart().invoke(null, (Object) param);
@@ -31,6 +34,10 @@ public class RuntimeEnv {
     }
 
     public void stopComp(String name){
+
+    }
+
+    public void getState(String name){
 
     }
 }
