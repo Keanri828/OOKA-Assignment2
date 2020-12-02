231e0b2e7cf884586a4d9a1e52576a1d1eabf796
Robin0815 <robin_frank_ccaa@yahoo.de>
2020-12-02 22:41:11 +0100

    alot

--- src/main/java/org.hbrs.ooka.ws2020.uebung2/view/CLI.java
+++ src/main/java/org.hbrs.ooka.ws2020.uebung2/view/CLI.java
@@ -1,5 +1,35 @@
 package org.hbrs.ooka.ws2020.uebung2.view;
 
-public class CLI{
+import org.hbrs.ooka.ws2020.uebung2.runtime.RuntimeEnv;
 
+import java.util.Scanner;
+
+public class CLI{
+    public void startCli()throws Exception{
+        RuntimeEnv re = new RuntimeEnv();
+        Scanner s = new Scanner(System.in);
+        System.out.println("New RuntimeEnviroment started: \nPlease type command (init, delete, start, stop, state) followed by the Component Name");
+        while(s.hasNext()){
+            String command = s.next();
+            if(command.equals("init")){
+                re.initComponent(s.next());
+            }
+            else if(command.equals("delete")){
+                re.deleteComponent(s.next());
+            }
+            else if(command.equals("start")){
+                re.startComp(s.next());
+            }
+            else if(command.equals("stop")){
+                re.stopComp(s.next());
+            }
+            else if(command.equals("state")){
+                re.getState(s.next());
+            }
+        }
+
+
+
+        s.close();
+    }
 }
