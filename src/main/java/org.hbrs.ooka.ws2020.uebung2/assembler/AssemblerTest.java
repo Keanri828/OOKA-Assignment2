86ae265aa57c5a99b329c640772d642a54a1ca18
Robin0815 <robin_frank_ccaa@yahoo.de>
2020-12-02 11:07:21 +0100

    back at it again

--- src/main/java/org.hbrs.ooka.ws2020.uebung2/assembler/AssemblerTest.java
+++ src/main/java/org.hbrs.ooka.ws2020.uebung2/assembler/AssemblerTest.java
@@ -14,6 +14,7 @@ import java.lang.ClassLoader.*;
 public class AssemblerTest {
     public static void main(String[] args) throws Exception {
         //System.out.println("Test n Stuff");
+
         ComponentContainer con = ComponentContainer.getInstance();
 
 
